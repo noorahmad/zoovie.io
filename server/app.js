e3c68a1ae8b5602090388a2717775924805690e3
noorahmad <noorahmad23@hotmail.com>
2017-08-10 19:25:20 -0700

    added authentication

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
 const Content = require('./models/content')
 const Comment = require('./models/comment')
+const User = require('./models/user')
 const seedDB = require('./seeds')
 
 mongoose.connect('mongodb://noor:noor@ds137197.mlab.com:37197/zoovie')
@@ -12,17 +15,37 @@ app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 seedDB()
 
+//Passport configuration
+app.use(require('express-session')({
+  secret: 'First car was made in 1996',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
+
+
 app.get('/', (req, res) => {
   res.render('landing')
 })
 
 
 app.get('/content', (req, res) => {
+  //Get all content from DB
   Content.find({}, (err, allcontent) => {
     if (err) {
       console.log(err)
     } else {
-      res.render('content/index', {content: allcontent})
+      res.render('content/index', {content: allcontent, currentUser: req.user})
     }
   })
 })
@@ -52,14 +75,13 @@ app.get('/content/:id', (req, res) => {
     if (err) {
       console.log(err)
     } else {
-      console.log(foundContent)
       res.render('content/show', {content: foundContent})
     }
   })
 })
 
 //Comment Route
-app.get('/content/:id/comments/new', (req, res) => {
+app.get('/content/:id/comments/new', isLoggedIn, (req, res) => {
   Content.findById(req.params.id, (err, content) => {
     if (err) {
       console.log(err)
@@ -69,7 +91,7 @@ app.get('/content/:id/comments/new', (req, res) => {
   })
 })
 
-app.post('/content/:id/comments', (req, res) => {
+app.post('/content/:id/comments', isLoggedIn, (req, res) => {
   Content.findById(req.params.id, (err, content) => {
     if (err) {
       console.log(err)
@@ -87,6 +109,54 @@ app.post('/content/:id/comments', (req, res) => {
     }
   })
 })
+//============
+//Auth routes
+//============
+
+//show register form
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+//handle sign up logic
+app.post('/register', (req, res) => {
+  const newUser = new User({username: req.body.username})
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err)
+      return res.render('register')
+    }
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/content')
+    })
+  })
+})
+
+//show login form
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+//handling login logic
+//app.post('/login', middleware, callback)
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: '/content',
+    failureRedirect: '/login'
+  }), (req, res) => {
+})
+
+//logout route
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/content')
+})
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
 
 app.listen(3000, () => {
   console.log('listening on 3000')
